9cb00d356c3998d151d6ed0efcc9111a8a2fd650
Manith Marapperuma <39-bce-0006@kdu.ac.lk>
2024-04-28 12:33:35 +0530

    Update Shared.kt

--- app/src/main/java/com/dbtechprojects/mydiary/ui/Shared.kt
+++ app/src/main/java/com/dbtechprojects/mydiary/ui/Shared.kt
@@ -1,34 +1,49 @@
 package com.dbtechprojects.mydiary.ui
 
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.dbtechprojects.mydiary.R
 
 @Composable
 fun GenericAppBar(
     title: String,
-    onIconClick: (() -> Unit)?,
-    icon: @Composable() (() -> Unit)?,
+    onIconClick: (() -> Unit)? = null,
+    onSettingsClick: (() -> Unit)? = null,
+    icon: @Composable (() -> Unit)? = null,
     iconState: MutableState<Boolean>
 ) {
     TopAppBar(
         title = { Text(title) },
         backgroundColor = MaterialTheme.colors.primary,
         actions = {
-            IconButton(
-                onClick = {
-                    onIconClick?.invoke()
-                },
-                content = {
-                    if (iconState.value){
-                        icon?.invoke()
+            // Conditionally display the main icon
+            if (iconState.value && icon != null) {
+                IconButton(
+                    onClick = {
+                        onIconClick?.invoke()
                     }
+                ) {
+                    icon()
                 }
-
-            )
+            }
+            // Conditionally display the settings icon
+            if (onSettingsClick != null) {
+                IconButton(
+                    onClick = {
+                        onSettingsClick.invoke()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = "Settings"
+                    )
+                }
+            }
         }
     )
 }
