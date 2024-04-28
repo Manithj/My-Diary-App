969cc8dd01dce2f46fa2bb2d57b0a058552064c1
Manith Marapperuma <39-bce-0006@kdu.ac.lk>
2024-04-28 12:33:29 +0530

    Update MainActivity.kt

--- app/src/main/java/com/dbtechprojects/mydiary/MainActivity.kt
+++ app/src/main/java/com/dbtechprojects/mydiary/MainActivity.kt
@@ -1,8 +1,22 @@
 package com.dbtechprojects.mydiary
-
+import androidx.compose.material.Button
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -61,8 +75,61 @@ class MainActivity : ComponentActivity() {
                 // Create Note Page
                 composable(Constants.NAVIGATION_NOTES_CREATE) { CreateNoteScreen(navController, notesViewModel) }
 
+                composable(route = Constants.NAVIGATION_SETTINGS) {
+                    SettingsScreen(context = this@MainActivity.applicationContext)
+                }
+
             }
 
+        }
+    }
+}
+
+//@Composable
+//fun SettingsScreen() {
+//    Surface(modifier = Modifier.fillMaxSize()) {
+//        Column(modifier = Modifier.padding(16.dp)) {
+//            Text("Settings", style = MaterialTheme.typography.h5)
+//            Spacer(modifier = Modifier.height(20.dp))
+//            Text("App Creator: Manith Jayaba")
+//            Text("Index No: D/BCE/22/0006")
+//            // Add more settings options as needed
+//        }
+//    }
+//}
+
+
+
+@Composable
+fun SettingsScreen(context: Context) {
+    val (title, setTitle) = remember { mutableStateOf("") }
+    val (updatedStr, setUpdatedStr) = remember {
+        mutableStateOf(context.resources.getString(R.string.name))
+    }
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text("Settings", style = MaterialTheme.typography.h5)
+            TextField(
+                value = title,
+                onValueChange = setTitle,
+                label = { Text("Enter New Name") }
+            )
+            Button(
+                onClick = {
+                    setUpdatedStr(title)
+                }
+            ) {
+                Text("Update String")
+            }
+            Text(title)
+            Text(updatedStr)
+            Text("App Creator: Manith Jayaba")
+            Text("Index No: D/BCE/22/0006")
+            // Add more settings options as needed
         }
     }
 }
